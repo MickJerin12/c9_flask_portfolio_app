61187e37030fed3c7d4a5d2e98cf38c4a5c96dde
Mick Jerin <s.mickjerin@outlook.com>
2018-03-15 21:38:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,6 +55,7 @@ def shopping_list_post():
               
               
             return render_template('shopping_list.html', result="\n".join([str(item) for item in shop_list]))
+	    print("hi")
 	    
           except ValueError:
             return "Easy now! Let's keep it simple! Just words with a space between them"
